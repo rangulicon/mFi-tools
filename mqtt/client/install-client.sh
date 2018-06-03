34966e4a9e7093ee219b087f099ef171d8a0ff46
maletazul <luissilva@maletazul.pt>
2018-06-03 19:18:50 +0100

    Repository Update

--- mqtt/client/install-client.sh
+++ mqtt/client/install-client.sh
@@ -2,7 +2,7 @@
 
 LOCALDIR="/var/etc/persistent/mqtt"
 LOCALSCRIPTDIR=$LOCALDIR/client
-BASEURL="https://raw.githubusercontent.com/magcode/mpower-tools/master/mqtt"
+BASEURL="https://raw.githubusercontent.com/maletazul/mFi-tools/master/mqtt"
 
 echo "Installing mPower MQTT v3 ..."
 wget --no-check-certificate -q $BASEURL/libmosquitto.so.1?raw=true -O $LOCALDIR/libmosquitto.so.1
